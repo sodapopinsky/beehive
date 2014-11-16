5e722231f411c7486d5f41a13d5db6f26163b43f
sodapopinsky <Nick@Nicholass-iMac.local>
2014-11-16 15:56:15 -0600

    f

--- app/views/messages/messages.blade.php
+++ app/views/messages/messages.blade.php
@@ -8,7 +8,6 @@
   <div class="cl-mcont aside" >    <div class="page-aside email" >
 
 
-
       <div class="">
         <div class="content" style="padding:10px;">
           <button class="navbar-toggle" data-target=".mail-nav" data-toggle="collapse" type="button">
@@ -20,6 +19,7 @@
 
         @foreach($data['conversations'] as $key => $conversation)
         <?php
+        if(!isset($initId)){
         if($key == $conversation->from){
            $initName = $conversation->fromUser()->first()->firstName . ' ' . $conversation->fromUser()->first()->lastName;
         }
@@ -28,13 +28,14 @@
         }
        
         $initId = $key;
+      }
         ?>
     
           <div style="background:#F2F2F2; cursor:pointer; margin-bottom:1px; padding:10px 5px;" onclick="goAjax({{$key}},'{{ $initName }}');"> 
 
         <span class="pull-right date">{{$conversation->created_at}}</span> 
         <img class="avatar pull-left" alt="user-avatar" src="images/avatar_50.jpg"/> 
-                  <span class="pull-left" style="margin-left:5px;"><b>{{$initName}}</b>
+                  <span class="pull-left" style="margin-left:5px;"><b>{{$conversation->toUser()->first()->firstName . ' ' . $conversation->toUser()->first()->lastName}}</b>
                   <p>{{$conversation->message}}</p> </span>
         
             <div style="clear:both"></div>      
@@ -52,11 +53,25 @@
     </div>  
 
 
- <div class="content">
+ <div class="content" >
+
+
 <div style="height:60px; padding:10px; background-color:#FFFFFF;">
         
                   <h4 id="username"><b></b></h4>
                    </span>
+      </div>
+
+    <div class="mail editor" style=" padding:5px; background-color:#FFF; border-bottom: 1px solid #E5E5E5">
+    <form method="post" action="/postmessage">
+    <input type="hidden" \ name="toUser" id="toUser">
+        <textarea style="height:100px;  " class="form-control" id="some-textarea" name="message" placeholder="Say something..."></textarea>
+        <div class="form-group" style="background-color:#FFF; height:40px;">
+          <button class="btn btn-primary pull-right" type="submit"><i class="fa fa-envelope"></i> Send</button>
+          </div>
+
+   
+        </form>
       </div>
 
     <div class="mail-inbox" id="ajaxContent">   
@@ -86,7 +101,7 @@
        $("#username").html(name);
    
    $.ajax({url:"messages/conversation/"+id,success:function(result){
-
+    $("#toUser").val(id);
     $("#ajaxContent").html(result);
   }});
 
